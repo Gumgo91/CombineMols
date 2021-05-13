fa390d22ae9e2fcc9292639cbeecc440aeafad66
Lucas Hyunseung Kong <antibody91@naver.com>
2021-05-13 13:36:25 +0900

    Initial commit

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+setup(name='CombineMols',
+
+version='1.0.0.0',
+
+description='easy to combine the two molecules',
+
+author='Hyunseung Kong',
+
+author_email='antibody91@naver.com',
+
+url='https://github.com/gumgo91',
+
+license='MIT',
+
+py_modules=['CombineMols'],
+
+python_requires='>=3',
+
+install_requires=['rdkit', 'mendeleev'],
+
+packages=find_packages(exclude = []),
+
+classifiers         = [
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+
+)
