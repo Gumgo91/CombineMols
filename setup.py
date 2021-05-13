7712c4ff7ba593badba3b82bd2070f80019a1c72
Lucas Hyunseung Kong <antibody91@naver.com>
2021-05-13 14:45:07 +0900

    Update setup.py
    add long_description

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(name='CombineMols',
 
-version='1.0.0.0',
+version='1.0.0.2',
 
 description='Easy to combine two molecules',
+
+long_description=long_description,
+long_description_content_type="text/markdown",
 
 author='Hyunseung Kong',
 
